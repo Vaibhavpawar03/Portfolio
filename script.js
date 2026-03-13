7533e86c39bd2de4c638fa0b9a7ef0591f23d785
Vaibhavpawar03 <vaibhavpawar2305@gmail.com>
2026-03-13 12:30:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.querySelectorAll("nav a").forEach(link => {
+
+link.addEventListener("click",function(e){
+
+e.preventDefault()
+
+const target=document.querySelector(this.getAttribute("href"))
+
+window.scrollTo({
+
+top:target.offsetTop-60,
+behavior:"smooth"
+
+})
+
+})
+
+})
